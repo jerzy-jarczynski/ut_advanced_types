8ce0861f14e56854f6f9ff6fa19619a01bdf6696
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-11-26 00:19:05 +0100

    Complete 83. Intersection Types

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,19 @@
-// Code goes here!
+type Admin = {
+  name: string;
+  privileges: string[];
+};
+
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+// interface ElevatedEmployee extends Employee, Admin {} <- interfaces inheritance alternative
+
+type ElevatedEmployee = Admin & Employee;
+
+const el: ElevatedEmployee = {
+  name: 'Max',
+  privileges: ['create-server'],
+  startDate: new Date()
+};
