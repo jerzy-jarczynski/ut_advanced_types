1c02a8462610da3866549dd9a06d62395a147feb
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-11-26 10:17:34 +0100

    Complete 87. Index Properties

--- src/app.ts
+++ src/app.ts
@@ -110,3 +110,12 @@ const userInputElement = document.getElementById('user-input');
 if (userInputElement) {
   (userInputElement as HTMLInputElement).value = 'Hi there!';
 }
+
+interface ErrorContainer { // { email: 'Not a valid email', username: 'Must start with a character' }
+  [prop: string]: string;
+}
+
+const errorBag: ErrorContainer = {
+  email: 'Not a valid email!',
+  username: 'Must start with a capital character!'
+};
